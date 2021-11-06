59f269ce3f71197841134e03bd43b375c70d0b98
ryamaoka-cq <74167612+ryamaoka-cq@users.noreply.github.com>
2021-11-06 19:33:37 +0900

    omikuji作成

---
+++ example/graph/schema.resolvers.go
@@ -0,0 +1,32 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"example/graph/generated"
+	"example/graph/model"
+	"fmt"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	// panic(fmt.Errorf("not implemented"))
+	return [&{ID: "id", Text: "test", Done: true, User: &{
+		ID: "id"
+	Name: "name"
+	}}]
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
